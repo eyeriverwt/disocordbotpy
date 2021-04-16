508e10431fccdcb1d19867c16af9031539bc2441
ai.kawakami <ai.kawakami@widetec.com>
2021-04-16 14:22:22 +0900

    test hello

--- discordbot.py
+++ discordbot.py
@@ -13,13 +13,26 @@ token = os.environ['DISCORD_BOT_TOKEN']
 # 接続に必要なオブジェクトを生成
 client = discord.Client()
 
-@client.event
-# botのログインが成功した後
+"""Bot起動時に実行されるイベントハンドラ"""
+@client.event # イベントを受信するための構文（デコレータ）
 async def on_ready():
     print('------Logged in as------')
     print(client.user.name)
     print(client.user.id)
     print('------------------------')
+
+
+# コマンドに対応するリストデータを取得する関数を定義
+def get_data(message):
+    command = message.content
+    data_table = {
+        '/members': message.guild.members, # メンバーのリスト
+        '/roles': message.guild.roles, # 役職のリスト
+        '/text_channels': message.guild.text_channels, # テキストチャンネルのリスト
+        '/voice_channels': message.guild.voice_channels, # ボイスチャンネルのリスト
+        '/category_channels': message.guild.categories, # カテゴリチャンネルのリスト
+    }
+    return data_table.get(command, '無効なコマンドです')
 
 
 
@@ -33,6 +46,15 @@ async def on_message(message):
     if message.content == '/neko':
         await message.channel.send('にゃーん')
 
-# Botの起動とDiscordサーバーへの接続
+    #if client.user in message.mentions: # 話しかけられたかの判定
 
+    if 'おは' in message.content:
+         await message.channel.send(client.user'ちゃん、おはゆ！')
+  
+    # コマンドに対応するデータを取得して表示
+    print(get_data(message))
+
+
+
+# Botの起動とDiscordサーバーへの接続
 client.run(token)
